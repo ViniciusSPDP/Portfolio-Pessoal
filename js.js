d9fadcc023f7b47d3d2ac2bbcecfa5f2f141d2fd
ViniciusSPDP <vinicius_saraiva2012@hotmail.com>
2023-06-12 00:30:04 -0300

    Quase Final

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+//Teste texto digitado
 var typed = new Typed('#element', {
     strings: ['Tecnólogias que comecei', 'estou aprimorando cada vez mais', 'Além de aprender novas tecnologias para ficar sempre atualizado'],
     typeSpeed: 50,
@@ -9,7 +10,30 @@ var typed = new Typed('#element', {
     cursorChar: '|',
 });
 
-window.addEventListener('scroll', function(){
+//Navbar Fixo
+window.addEventListener('scroll', function () {
     var navbar = document.querySelector('.none')
     navbar.classList.toggle('mudarnav', window.scrollY > 650)
 })
+
+//Validar email
+function validarFormulario(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("email").value;
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regexEmail.test(email)) {
+        alert("Email inválido!");
+        let btn = document.querySelector('#enviar')
+        btn.setAttribute("disabled", "");
+        btn.setAttribute("style", "background-color: gray;")
+        return false;
+    }
+
+    let btn = document.querySelector('#enviar')
+    btn.removeAttribute("disabled", "");
+    btn.removeAttribute("style", "background-color: gray;")
+    btn.setAttribute("style", "cursor: pointer;")
+    return true;
+}
