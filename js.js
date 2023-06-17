3cd83abaac76fcf8d4b19ddd136e3c122379c1bd
ViniciusSPDP <vinicius_saraiva2012@hotmail.com>
2023-06-17 13:36:44 -0300

    Final

--- js.js
+++ js.js
@@ -16,24 +16,38 @@ window.addEventListener('scroll', function () {
     navbar.classList.toggle('mudarnav', window.scrollY > 650)
 })
 
+var validEmail = false;
+
 //Validar email
 function validarFormulario(event) {
     event.preventDefault();
 
-    var email = document.getElementById("email").value;
-    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!regexEmail.test(email)) {
+    if (!validEmail) {
         alert("Email inválido!");
-        let btn = document.querySelector('#enviar')
-        btn.setAttribute("disabled", "");
-        btn.setAttribute("style", "background-color: gray;")
         return false;
     }
 
+    return true;
+}
+
+function checkMail(){
+    var email = document.getElementById("email").value;
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    console.log(regexEmail.test(email))
+
+
+    if (!regexEmail.test(email)) {
+
+        let btn = document.querySelector('#enviar')
+        btn.setAttribute("disabled", "");
+        btn.setAttribute("style", "background-color: gray;")
+        validEmail = false;
+
+    }else{
     let btn = document.querySelector('#enviar')
     btn.removeAttribute("disabled", "");
     btn.removeAttribute("style", "background-color: gray;")
     btn.setAttribute("style", "cursor: pointer;")
-    return true;
+    validEmail = true;
+}
 }
